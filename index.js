214e3c8a5652353efd366a0ba204b3b823b49b32
Denis <denis.were@student.moringaschool.com>
2023-04-11 18:26:31 +0300

    complete

--- index.js
+++ index.js
@@ -1,34 +1,76 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  
+  const complements = new Set();
+  for (const number of array) {
+    if (complements.has(number)) {
+      return true;
+    }
+    complements.add(target - number);
+  }
+  return false;
+
+
 }
 
 /* 
-  Write the Big O time complexity of your function here
+This implementation has a time complexity of O(n), where n is the length of the input array, since we only need to iterate over the array once and perform constant-time operations inside the loop. The space complexity is also O(n), since in the worst case we need to store all the complements in the complements set.
 */
 
 /* 
-  Add your pseudocode here
+function hasTargetSum(array, target):
+   Initialize an empty set to store complements
+  complements = new Set()
+  
+   Iterate over the elements of the array
+  for number in array:
+       Check if the current number's complement is in the set
+      if complements.contains(number):
+           We have found a pair that adds up to the target
+          return true
+      
+       Add the complement of the current number to the set
+      complements.add(target - number)
+  
+   We have checked all possible pairs and none add up to the target
+  return false
+
 */
 
 /*
-  Add written explanation of your solution here
+We initialize an empty set called complements. This set will store the complements of the numbers we have seen so far (i.e., the numbers we need to reach the target sum).
+We iterate over the numbers in the array.
+For each number, we check if its complement is already in the complements set. If it is, we have found a pair of numbers that add up to the target, so we return true.
+Otherwise, we add the complement of the current number to the complements set.
+If we have iterated over all the numbers in the array and haven't found a pair that adds up to the target, we return false.
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+console.log("Expecting: false");
+console.log("=>", hasTargetSum([1, 2, 5], 4));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([-2, 0, 3, -5, 2, 7, 1], -2));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+console.log("Expecting: false");
+console.log("=>", hasTargetSum([1, 2, 5], 10));
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+
+console.log("");
+
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
